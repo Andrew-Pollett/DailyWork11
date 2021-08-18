fc99acb5df9b244faadbc1cde1e7e9cabf449c1e
andrew.pollett <ARPollett@gmail.com>
2021-08-18 11:44:38 -0230

    Version 3

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ y = []
 numberOfX = int(input("Enter the number of x values"))
 for q in range(numberOfX):
     x.append(q+1)
-y = []
 for i in range(len(x)):
     y.append(math.sin(i+1))
 plt.plot(x, y, color = "green", marker = ".")
